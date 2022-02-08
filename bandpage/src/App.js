e2c9c28f7c7daa47df02a000523d9265eb37915a
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-02-07 16:21:46 -0800

    g

--- bandpage/src/App.js
+++ bandpage/src/App.js
@@ -9,7 +9,7 @@ function App() {
       
       <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
 
       </Routes>
