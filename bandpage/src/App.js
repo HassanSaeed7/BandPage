e2f5694b0c4ff60c0021cd70534d2df5814db028
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-02-20 02:51:53 -0800

    test deploy

--- bandpage/src/App.js
+++ bandpage/src/App.js
@@ -9,7 +9,7 @@ function App() {
       
       <BrowserRouter>
       <Routes>
-      <Route exact element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       </Routes>
       </BrowserRouter>
